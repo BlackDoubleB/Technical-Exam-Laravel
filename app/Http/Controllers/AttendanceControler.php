65bbbd78a08d2af74550b645e06948cefed90d6a
BlackDoubleB <blancablacido@gmail.com>
2026-05-04 15:10:31 -0500

    agregando authcontroller

---
+++ app/Http/Controllers/AttendanceControler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AttendanceControler extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Credenciales incorrectas'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
